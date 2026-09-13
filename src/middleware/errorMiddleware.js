71916f3ae19c645c2b6c84b31bdb3eeedcd58de1
ahmed-elrooby <ahmdalrwby24@gmail.com>
2026-09-13 22:34:14 +0300

    error handling

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -1,6 +1,20 @@
+
 const errorMiddleware = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
   let message = err.message || "حدث خطأ في السيرفر";
+
+  // MongoDB Duplicate Key Error
+  if (err.code === 11000) {
+    statusCode = 409;
+
+    const field = Object.keys(err.keyValue || {})[0];
+
+    if (field === "email") {
+      message = "البريد الإلكتروني مستخدم بالفعل";
+    } else {
+      message = "البيانات موجودة بالفعل";
+    }
+  }
 
   // MongoDB ObjectId غير صالح
   if (err.name === "CastError") {
@@ -8,10 +22,11 @@ const errorMiddleware = (err, req, res, next) => {
     message = "معرف غير صالح";
   }
 
-  res.status(statusCode).json({
+  return res.status(statusCode).json({
     success: false,
     message,
   });
 };
 
 export default errorMiddleware;
+
